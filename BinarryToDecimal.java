8e33c0035abd1180817021b866b9cfe5b284c1ea
Vishal Ghuge <115221847+ghuge637@users.noreply.github.com>
2026-03-01 22:13:32 +0530

    Implement binary to decimal conversion

---
+++ BinarryToDecimal.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class BinarryToDecimal
+{
+	public static void main(String[] args) {
+	    int num = 0;
+	    String binary = "1010";
+	    int p = 0;
+	    
+	    for(int i = binary.length()-1; i>=0; i--){
+	        if(binary.charAt(i) == '1'){
+	            num +=(int) Math.pow(2,p);
+	            p++;
+	        }else{
+	            p++;
+	        }
+	    }
+	    
+		System.out.println(num);
+	}
+}
