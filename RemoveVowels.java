348175fa5553428e069ac166243b6e18940fad42
Vishal Ghuge <115221847+ghuge637@users.noreply.github.com>
2026-03-03 23:49:25 +0530

    Create RemoveVowels.java

---
+++ RemoveVowels.java
@@ -0,0 +1,14 @@
+public class RemoveVowels
+{
+	public static void main(String[] args) {
+	   String st = "welcome to geeksforgeeks";
+
+        for(int i=0; i<st.length();i++){
+            if(st.charAt(i)=='a' || st.charAt(i)=='e' || st.charAt(i)=='i' || st.charAt(i)=='o' || st.charAt(i)=='u' || st.charAt(i)=='A' || st.charAt(i)=='E' || st.charAt(i)=='I' || st.charAt(i)=='O' || st.charAt(i)=='U'){
+                continue;
+            }else{
+                System.out.print(st.charAt(i));
+            }
+        }
+	}
+}
