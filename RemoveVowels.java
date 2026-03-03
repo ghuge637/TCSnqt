f7135cf2dd8648290dd501e20a7f1227fc13289d
Vishal Ghuge <115221847+ghuge637@users.noreply.github.com>
2026-03-03 23:52:54 +0530

    Change input string in RemoveVowels class

--- RemoveVowels.java
+++ RemoveVowels.java
@@ -1,7 +1,7 @@
 public class RemoveVowels
 {
 	public static void main(String[] args) {
-	   String st = "welcome to geeksforgeeks";
+	   String st = "welcome to CollAge";
 
         for(int i=0; i<st.length();i++){
             if(st.charAt(i)=='a' || st.charAt(i)=='e' || st.charAt(i)=='i' || st.charAt(i)=='o' || st.charAt(i)=='u' || st.charAt(i)=='A' || st.charAt(i)=='E' || st.charAt(i)=='I' || st.charAt(i)=='O' || st.charAt(i)=='U'){
