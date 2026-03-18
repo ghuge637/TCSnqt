7f57aeeb7e9431983ec232562ac2d8bf91af9ea0
Vishal Ghuge <115221847+ghuge637@users.noreply.github.com>
2026-03-18 23:53:57 +0530

    Create Sum_of_arithmetic_series.java

---
+++ Sum_of_arithmetic_series.java
@@ -0,0 +1,12 @@
+
+public class Sum_of_arithmetic_series
+{
+	public static void main(String[] args) {
+	   float a = 2.5f, d = 1.5f, n=20f;
+	    
+	   float sum = (n / 2) * (2 * a + (n - 1) * d);
+	    
+	    System.out.print(sum);
+	    
+	}
+}
