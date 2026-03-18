8a1c99902343031973ce84b474bcf2aaf3caff1f
Vishal Ghuge <115221847+ghuge637@users.noreply.github.com>
2026-03-19 00:12:41 +0530

    Create Replace.java

---
+++ Replace.java
@@ -0,0 +1,10 @@
+public class Replace
+{
+	public static void main(String[] args) {
+	    
+	   int num = 102;
+	   
+	   String result = String.valueOf(num).replace('0', '5');
+	   System.out.print(Integer.parseInt(result));
+	}
+}
