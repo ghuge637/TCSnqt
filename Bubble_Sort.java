52709c5f9d1ab6e590667046b44c4f1438f3bc9d
Vishal Ghuge <115221847+ghuge637@users.noreply.github.com>
2026-03-21 00:33:42 +0530

    Create Bubble_Sort.java

---
+++ Bubble_Sort.java
@@ -0,0 +1,22 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] arr = {4,6,5,1,3,2};
+	    
+	    for(int i=0; i<arr.length; i++){
+	        for(int j=0; j<arr.length; j++){
+	            if(arr[j]>arr[i]){
+	                int temp = arr[i];
+	                arr[i] = arr[j];
+	                arr[j] = temp;
+	            }
+	        }
+	    }
+	    
+	    for(int i=0; i<arr.length; i++){
+	        System.out.print(arr[i]+" ");
+	    }
+	}
+}
